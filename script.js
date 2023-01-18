054031459956c9461f88bc3eb76def741ac131ef
Your Name <saghafy.80@gmail.com>
2023-01-19 03:29:29 +0330

    Implement filter section

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 'user strict';
+
+const continentListButton = document.querySelector('.select-continent-area button');
+const continentListIcon = continentListButton.querySelector('i');
+const continentList = document.querySelector('.continent-list');
+const selectedContinent = document.getElementById('selected-continent');
+
+const openContinentList = () => {
+    continentList.classList.add('open');
+    continentListIcon.style.transform = 'rotateZ(180deg)';
+}
+
+const closeContinentList = () => {
+    continentList.classList.remove('open');
+    continentListIcon.style.transform = 'rotateZ(0deg)';
+}
+
+continentListButton.addEventListener('click', openContinentList);
+
+document.addEventListener('click', (e) => {
+
+    if (continentList.classList.contains('open')) {
+
+        if (e.target.closest('.continent-list')) {
+
+            selectedContinent.innerHTML = e.target.innerText;
+        }
+        else if (e.target.closest('.select-continent-area')) {
+            return;
+        }
+        closeContinentList();
+    }
+});
